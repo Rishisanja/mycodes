a273a1236079befd54f6afd2a920b6ba2e884335
Rishisanja <sanjarishi99@gmail.com>
2024-07-30 22:41:42 +0530

    Add files via upload

---
+++ 2nd perimeter.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+
+int length,breadth;
+
+length=50;
+breadth=30;
+
+int perimeter= length*breadth;
+printf("The perimeter of the given plot is = %d",perimeter);
+
+return 0;
+
+
+}
